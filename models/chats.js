73caeb944df1d79fe91ff12ca322180ea7309d58
Siddhant Jha <siddhantjha@Siddhants-MacBook-Air.local>
2025-12-29 11:21:44 +0530

    added time log in ist

--- models/chats.js
+++ models/chats.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+const moment = require("moment-timezone");
 
-const chatSchema = new mongoose.Schema(
-  {
-    from: String,
-    message: String,
-    direction: String
-  },
-  { timestamps: true }
-);
+const chatSchema = new mongoose.Schema({
+  from: String,
+  message: String,
+  direction: String,
+
+  createdAt: {
+    type: Date,
+    default: () => moment().tz("Asia/Kolkata").toDate()
+  }
+});
 
 module.exports = mongoose.model("Chat", chatSchema);
